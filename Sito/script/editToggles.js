baedc54fd277eec2f54d318f9ea41cf3775655f4
Ro0t-set <tommaso.patriti@studio.unibo.it>
2021-11-27 10:33:21 +0100

    select lenguages

--- Sito/script/editToggles.js
+++ Sito/script/editToggles.js
@@ -3,6 +3,10 @@ $(document).on('ready', function (){
         quadro.setStyle($('#style').val());
     }));
 
+    $('#language').on('change', (function () {
+        quadro.setLanguages($('#language').val());
+    }));
+
     $('#frame-color').on('change', (function () {
         quadro.setFramecolor($('#frame-color').val());
     }));
