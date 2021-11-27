094adf2750ebf0b43ebffe187d7763339ad41b5c
Ro0t-set <tommaso.patriti@studio.unibo.it>
2021-11-27 10:21:56 +0100

    frame color

--- Sito/script/editToggles.js
+++ Sito/script/editToggles.js
@@ -2,5 +2,12 @@ $(document).on('ready', function (){
     $('#style').on('change', (function () {
         quadro.setStyle($('#style').val());
     }));
+
+    $('#frame-color').on('change', (function () {
+        quadro.setFramecolor($('#frame-color').val());
+    }));
 });
+
+
+
 
