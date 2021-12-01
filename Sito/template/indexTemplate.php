76e0a279e022df0addb492e54370dcebf27b12f3
StefanoScolari <stefano.scolari@studio.unibo.it>
2021-12-02 00:57:58 +0100

    barra categorie

--- Sito/template/indexTemplate.php
+++ Sito/template/indexTemplate.php
@@ -1,37 +1,37 @@
-<div class="row flex-row flex-nowrap mt-4 pb-4 pt-2">
+<div class="container-fluid">
+    <div class="row flex-row flex-nowrap">
+        <div class="col-3">
+            <div class="card card-block">
+                <img class="card-img-top" src="img/logos/videogame.png" alt="Card image cap">
+                <div class="class-footer">
+                    <p class="card-text">Videogames</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card card-block">
+                <img class="card-img-top" src="img/logos/pi.png" alt="Card image cap">
+                <div class="class-footer">
+                    <p class="card-text">Videogames</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card card-block">
+                <img class="card-img-top" src="img/logos/fisica.png" alt="Card image cap">
+                <div class="class-footer">
+                    <p class="card-text">Videogames</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card card-block">
+                <img class="card-img-top" src="img/logos/shuttle.png" alt="Card image cap">
+                <div class="class-footer">
+                    <p class="card-text">Videogames</p>
+                </div>
 
-    <div class="col-5 align-items-end">
-        <article>
-            <header>
-                <img src="../img/logos/videogame.png" class="img-fluid" alt="Responsive image">
-            </header>
-            <footer><p>Videogames</p></footer>
-        </article>
+            </div>
+        </div>
     </div>
-    <div class="col-5">
-        <article>
-            <header>
-                <img src="../img/logos/pi.png" class="img-fluid" alt="Responsive image">
-            </header>
-            <footer><p>Math</p></footer>
-        </article>
-    </div>
-    <div class="col-5">
-        <article>
-            <header>
-                <img src="../img/logos/fisica.png" class="img-fluid" alt="Responsive image">
-            </header>
-            <footer><p>Physics</p></footer>
-        </article>
-    </div>
-    <div class="col-5">
-        <article>
-            <header>
-                <img src="../img/logos/shuttle.png" class="img-fluid" alt="Responsive image">
-            </header>
-            <footer><p>Aerospace</p></footer>
-        </article>
-
-    </div>
-
 </div>
