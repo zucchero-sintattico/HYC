b5fbee8ae4aa302d036ca48bb09c2348eba6b9ca
StefanoScolari <stefano.scolari@studio.unibo.it>
2021-12-02 01:28:09 +0100

    almost there

--- Sito/template/indexTemplate.php
+++ Sito/template/indexTemplate.php
@@ -1,34 +1,34 @@
 <div class="container-fluid">
     <div class="row flex-row flex-nowrap">
-        <div class="col-3">
+        <div class="col-3 d-flex justify-content-center text-center">
             <div class="card card-block">
                 <img class="card-img-top" src="img/logos/videogame.png" alt="Card image cap">
-                <div class="class-footer">
+                <div class="class-footer mt-auto">
                     <p class="card-text">Videogames</p>
                 </div>
             </div>
         </div>
-        <div class="col-3">
+        <div class="col-3 d-flex justify-content-center text-center">
             <div class="card card-block">
                 <img class="card-img-top" src="img/logos/pi.png" alt="Card image cap">
-                <div class="class-footer">
-                    <p class="card-text">Videogames</p>
+                <div class="class-footer mt-auto">
+                    <p class="card-text">Maths</p>
                 </div>
             </div>
         </div>
-        <div class="col-3">
+        <div class="col-3 d-flex justify-content-center text-center">
             <div class="card card-block">
                 <img class="card-img-top" src="img/logos/fisica.png" alt="Card image cap">
-                <div class="class-footer">
-                    <p class="card-text">Videogames</p>
+                <div class="class-footer mt-auto">
+                    <p class="card-text">Physics</p>
                 </div>
             </div>
         </div>
-        <div class="col-3">
+        <div class="col-3 d-flex justify-content-center text-center">
             <div class="card card-block">
                 <img class="card-img-top" src="img/logos/shuttle.png" alt="Card image cap">
-                <div class="class-footer">
-                    <p class="card-text">Videogames</p>
+                <div class="class-footer mt-auto">
+                    <p class="card-text">Aerospace</p>
                 </div>
 
             </div>
