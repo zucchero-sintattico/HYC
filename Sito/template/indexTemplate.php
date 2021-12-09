56751330b71e9833f922a8508b8e1ef1b6a688ea
StefanoScolari <stefano.scolari@studio.unibo.it>
2021-12-10 00:31:41 +0100

    Modified DB, changed index.php and added queries

--- Sito/template/indexTemplate.php
+++ Sito/template/indexTemplate.php
@@ -5,54 +5,18 @@
 
     <div class="container-fluid">
         <div class="row flex-row flex-nowrap">
-            <div class="col-3 d-flex justify-content-center text-center">
-                <div class="card card-block">
-                    <img class="card-img-top" src="img/logos/videogame.png" alt="Card image cap">
-                    <div class="class-footer mt-auto">
-                        <p class="card-text">Videogames</p>
+            <?php foreach($templateParams['categories'] as $categoria): ?>
+                <div class="col-3 d-flex justify-content-center text-center">
+
+                    <div class="card card-block">
+                        <img class="card-img-top" src="img/logos/<?php echo $categoria['Tipo']?>.png" alt="Card image cap">
+                        <div class="class-footer mt-auto">
+                            <p class="card-text"><?php echo $categoria['Tipo']?></p>
+                        </div>
                     </div>
+
                 </div>
-            </div>
-            <div class="col-3 d-flex justify-content-center text-center">
-                <div class="card card-block">
-                    <img class="card-img-top" src="img/logos/pi.png" alt="Card image cap">
-                    <div class="class-footer mt-auto">
-                        <p class="card-text">Maths</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-3 d-flex justify-content-center text-center">
-                <div class="card card-block">
-                    <img class="card-img-top" src="img/logos/fisica.png" alt="Card image cap">
-                    <div class="class-footer mt-auto">
-                        <p class="card-text">Physics</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-3 d-flex justify-content-center text-center">
-                <div class="card card-block">
-                    <img class="card-img-top" src="img/logos/shuttle.png" alt="Card image cap">
-                    <div class="class-footer mt-auto">
-                        <p class="card-text">Aerospace</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-3 d-flex justify-content-center text-center">
-                <div class="card card-block">
-                    <img class="card-img-top" src="img/logos/shuttle.png" alt="Card image cap">
-                    <div class="class-footer mt-auto">
-                        <p class="card-text">Aerospace</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-3 d-flex justify-content-center text-center">
-                <div class="card card-block">
-                    <img class="card-img-top" src="img/logos/shuttle.png" alt="Card image cap">
-                    <div class="class-footer mt-auto">
-                        <p class="card-text">Aerospace</p>
-                    </div>
-                </div>
-            </div>
+            <?php endforeach; ?>
         </div>
     </div>
 
@@ -60,16 +24,39 @@
 
 <section>
     <header>
-        <h1>Most Popular</h1>
+        <h1>Most Popular Languages</h1>
     </header>
 
     <div class="container-fluid">
         <div class="row flex-row flex-nowrap">
             <?php foreach($templateParams['languages'] as $language): ?>
                 <div class="col-3 d-flex justify-content-center text-center">
+
                     <div class="row justify-content-center">
                         <?php echo $language['NomeLinguaggio'];?>
                     </div>
+
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+
+</section>
+
+<section>
+    <header>
+        <h1>Most Popular Codes</h1>
+    </header>
+
+    <div class="container-fluid">
+        <div class="row flex-row flex-nowrap">
+            <?php foreach($templateParams['mostPopularProducts'] as $popularProduct): ?>
+                <div class="col-3 d-flex justify-content-center text-center">
+
+                    <div class="row justify-content-center">
+                        <?php echo $popularProduct['titolo'];?>
+                    </div>
+
                 </div>
             <?php endforeach; ?>
         </div>
