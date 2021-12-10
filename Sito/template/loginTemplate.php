077908b7a54f29d8e026cc367bdc3cd2c0438599
Ro0t-set <tommaso.patriti@studio.unibo.it>
2021-12-10 14:02:49 +0100

    search bar selector js change

---
+++ Sito/template/loginTemplate.php
@@ -0,0 +1,18 @@
+
+    <form action="#" method="POST">
+        <h2>Login</h2>
+        <?php if (isset($templateParams["errorelogin"])): ?>
+            <p><?php echo $templateParams["errorelogin"]; ?></p>
+        <?php endif; ?>
+        <ul>
+            <li>
+                <label for="username">Username:</label><input type="text" id="username" name="username"/>
+            </li>
+            <li>
+                <label for="password">Password:</label><input type="password" id="password" name="password"/>
+            </li>
+            <li>
+                <input type="submit" name="submit" value="Invia"/>
+            </li>
+        </ul>
+    </form>
