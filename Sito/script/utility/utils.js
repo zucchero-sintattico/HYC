7b09b47109e20f6b6bb040879827b5fca4e46bf0
Ro0t-set <tommaso.patriti@studio.unibo.it>
2021-12-21 15:48:09 +0100

    bug fix resize

--- Sito/script/utility/utils.js
+++ Sito/script/utility/utils.js
@@ -1,14 +1,29 @@
 let quadri = [];
+
 function checkOnResize() {
-        if($(window).width()<768){
+    if ($(window).width() < 768) {
+        $('code').each(function () {
+            $(this).parent().removeClass("col-5");
+            $(this).parent().addClass("col");
+        });
+    } else {
+        $('code').each(function () {
+            $(this).parent().removeClass("col");
+            $(this).parent().addClass("col-5");
+        });
+    }
+    $(window).on('resize', function () {
+        if ($(window).width() < 768) {
             $('code').each(function () {
                 $(this).parent().removeClass("col-5");
                 $(this).parent().addClass("col");
             });
-        }else{
+        } else {
             $('code').each(function () {
                 $(this).parent().removeClass("col");
                 $(this).parent().addClass("col-5");
             });
         }
+
+    });
 }
