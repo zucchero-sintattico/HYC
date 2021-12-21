f6e5cab530fe21f03617738f47c324f797495421
Ro0t-set <tommaso.patriti@studio.unibo.it>
2021-12-21 13:38:40 +0100

    search fix, now don't exist the duplication

--- Sito/script/search/searchBar.js
+++ Sito/script/search/searchBar.js
@@ -1,29 +1,23 @@
-
 $(document).on('ready', function () {
     let backMainSetted = false;
-    let timeOut = true;
     let backmain = "";
 
     $("#searchField").on('input', function (event) {
         event.preventDefault();
-        if(!backMainSetted){
+        if (!backMainSetted) {
             backmain = "";
             backmain = $("main").html();
             backMainSetted = true;
         }
         let main = $("main");
         main.html("");
-        if ($("#searchField").val() !== "" && timeOut) {
-            timeOut = false;
+        if ($("#searchField").val() !== "") {
+
             $.getJSON("/API/api-search.php?key=" + $("#searchField").val(), function (data) {
                 let articoli = data["Results"];
                 const main = $("main");
-                if(articoli !== ""){
-                    let risultatiRicerca = getFilteredArticles(articoli, data["Title"]);
-                    main.html(risultatiRicerca);
-                }else {
-                    main.html("<h2>not found</h2>");
-                }
+                let risultatiRicerca = getFilteredArticles(articoli, data["Title"]);
+                main.html(risultatiRicerca);
 
             });
         } else {
@@ -31,7 +25,6 @@ $(document).on('ready', function () {
             const main = $("main");
             main.html(backmain);
         }
-        timeOut = true;
     });
 
 
