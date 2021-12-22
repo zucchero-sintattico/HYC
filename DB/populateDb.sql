feb7bc112a747d5355d774dc334898860b2aab14
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2021-12-22 17:57:47 +0100

    fix db

--- DB/populateDb.sql
+++ DB/populateDb.sql
@@ -8,7 +8,7 @@ INSERT INTO `Linguaggio` (`NomeLinguaggio`) VALUES ('clike'), ('python'), ('java
 INSERT INTO `Tema` (`NomeTema`) VALUES ('monokai'), ('base16-light');
 
 INSERT INTO `Prodotto` (`IdProd`, `Codice`, `Colore_frame`, `Larghezza`, `Titolo`, `Descrizione`, `Altezza`, `Padding`, `Dimensione_font`, `Mostra_numero_linee`, `NomeLinguaggio`, `IdCategoria`, `NomeTema`, `IdProdInVetrina`)
-VALUES (NULL, 'let a = 5;', 'red', '100', 'Apollo11', 'Primo codice usato per navicella', '100', '2', '6', 't', 'javascript', '4', 'base16-light', NULL),
-       (NULL, 'int a = 7;', 'blue', '120', 'Donut C-like', 'Codice C a forma di donut che renderizza donut 3d', '120', '3', '6', 't', 'clike', '3', 'base16-light', NULL);
+VALUES (NULL, 'let a = 5;', 'red', '100', 'Apollo11', 'Primo codice usato per navicella', '100', '2', '6', 't', 'javascript', '4', 'base16-light'),
+       (NULL, 'int a = 7;', 'blue', '120', 'Donut C-like', 'Codice C a forma di donut che renderizza donut 3d', '120', '3', '6', 't', 'clike', '3', 'base16-light');
 
 INSERT INTO `ProdottoInVetrina` (`IdProdInVetrina`, `IdProd`, `IndicePopolarita`) VALUES (NULL, '1', '1'), (NULL, '2', '2');
