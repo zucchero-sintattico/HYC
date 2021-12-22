4170798b9e7a3f8c0afad5fc7134a7fcbbed68ca
StefanoScolari <stefano.scolari@studio.unibo.it>
2021-12-22 15:01:07 +0100

    working on animazioni

--- Sito/script/editor/utils.js
+++ Sito/script/editor/utils.js
@@ -1,5 +1,42 @@
+function getRelativeOffset(start, end) {
+    const starting = start.getBoundingClientRect();
+    const finish = end.getBoundingClientRect();
+    return {
+        left: finish.left - starting.left,
+        top: finish.top - starting.top
+    }
+}
+
 $(document).on('ready', function () {
 
+    $("main > div > div > div > button").on("click", function (event) {
+        let stuffToTraslate = [];
+
+        stuffToTraslate.push(document.querySelector(".col-11 > div:nth-child(1) > div:nth-child(1)"));
+        stuffToTraslate.push(document.querySelector(".col-11 > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > code:nth-child(1)"))
+
+
+        for (let i = 0; i < stuffToTraslate.length; i++) {
+
+            let relativePosition = getRelativeOffset(stuffToTraslate[i], this);
+
+            stuffToTraslate[i].prototype.promise().done(function(){console.log("aaa");});
+            stuffToTraslate[i].animate([
+                {
+                    transform: "translate(0px,0px) scale(1,1)",
+                    opacity: 1
+                },
+                {
+                    transform: "translate(" + relativePosition.left + "px," + relativePosition.top + "px) scale(0.1, 0.1)",
+                    opacity: 0
+                }], 500, function () {
+                console.log("aaa");
+            })
+
+
+        }
+
+    });
 
     quadro.setStyle($('#style').val());
     quadro.setFramecolor($('#frame-color').val());
@@ -39,6 +76,5 @@ $(document).on('ready', function () {
 
 
 });
-
 
 
