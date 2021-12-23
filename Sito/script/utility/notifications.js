d6e7e44880ced1949c845d3a1237e3190cbcb51b
Ro0t-set <tommaso.patriti@studio.unibo.it>
2021-12-23 22:30:35 +0100

    add notification dropdown menu

--- Sito/script/utility/notifications.js
+++ Sito/script/utility/notifications.js
@@ -27,16 +27,16 @@ $(document).on('ready', function () {
     notificationToggle.on('click', function () {
 
         if (notificationDropDown == null) {
-            notificationToggle.append("<div></div>");
+            notificationToggle.append("<div><p>notifica 1</p><p>notifica 2</p></div>");
             notificationDropDown = notificationToggle.children().next().next();
             notificationDropDown.css("position", "absolute");
             notificationDropDown.css("background", "#f1f1f1")
 
             if($(window).width() < 768){
-                notificationDropDown.css("top", -$(window).height()+header.height() );
+                notificationDropDown.css("bottom", $("nav").height());
                 notificationDropDown.css("right", 0);
-                notificationDropDown.css("width", "95%");
-                notificationDropDown.css("height", $(window).height()-header.height());
+                notificationDropDown.css("width", "100%");
+                notificationDropDown.css("height", $(window).height()-100);
             }else{
                 notificationDropDown.css("width", 200);
                 notificationDropDown.css("height", 300);
