fc72f514799887a4330601a5471f85937fb773d2
Ro0t-set <tommaso.patriti@studio.unibo.it>
2021-12-23 22:12:31 +0100

    add notification dropdown menu

--- Sito/script/utility/notifications.js
+++ Sito/script/utility/notifications.js
@@ -8,8 +8,8 @@ $(document).on('ready', function () {
         };
         ws.onmessage = function (e) {
             console.log(e.data);
-            if(e.data === "update_notification"){
-                $.post( "/API/api-notification.php", function( data ) {
+            if (e.data === "update_notification") {
+                $.post("/API/api-notification.php", function (data) {
                     console.log("notifiche = ", data)
 
 
@@ -20,12 +20,37 @@ $(document).on('ready', function () {
     }
 
 
-
     //notification selector
     const notificationToggle = $("body > div > div:nth-child(2) > div > nav > ul > li:nth-child(5) > a");
-    notificationToggle.on('click', function (){
-        notificationToggle.append("<div style='width: 300px; height: 400px; position: absolute; right: 10px; float: end; background-color: #f1f1f1'></div>")
+    let notificationDropDown = null;
+    notificationToggle.on('click', function () {
+
+        if (notificationDropDown == null) {
+            notificationToggle.append("<div></div>");
+            notificationDropDown = notificationToggle.children().next().next();
+            notificationDropDown.css("position", "absolute");
+            notificationDropDown.css("background", "#f1f1f1")
+
+            if($(window).width() < 768){
+                notificationDropDown.css("top", -$(window).height()+30);
+                notificationDropDown.css("right", 0);
+                notificationDropDown.css("width", "95%");
+                notificationDropDown.css("height", $(window).height()-30);
+            }else{
+                notificationDropDown.css("width", 200);
+                notificationDropDown.css("height", 300);
+                notificationDropDown.css("top", 60);
+                notificationDropDown.css("right", 30);
+            }
+
+        } else {
+            notificationDropDown.remove();
+            notificationDropDown = null;
+        }
+
     })
+
+
 });
 
 
