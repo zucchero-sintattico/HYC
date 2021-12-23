c02e860adbfa0e35f6f2b8ad9da53b729194a3c1
Ro0t-set <tommaso.patriti@studio.unibo.it>
2021-12-23 19:59:15 +0100

    add notification dropdown menu

--- Sito/script/utility/notifications.js
+++ Sito/script/utility/notifications.js
@@ -18,6 +18,14 @@ $(document).on('ready', function () {
             }
         }
     }
+
+
+
+    //notification selector
+    const notificationToggle = $("body > div > div:nth-child(2) > div > nav > ul > li:nth-child(5) > a");
+    notificationToggle.on('click', function (){
+        $("main").prepend("<div style='width: 300px; height: 400px; position: absolute; right: 10px; float: end; background-color: #f1f1f1'></div>")
+    })
 });
 
 
