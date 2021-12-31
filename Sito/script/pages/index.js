32b38f1c8a1c0a1bcc8ebed02ef4afdfde4d6eb4
StefanoScolari <stefano.scolari@studio.unibo.it>
2021-12-31 02:36:34 +0100

    Finish

--- Sito/script/pages/index.js
+++ Sito/script/pages/index.js
@@ -3,6 +3,7 @@ $(document).on("ready", function (event) {
     event.preventDefault();
     $.getJSON("/API/api-homepage.php", function (data) {
         fillHomePage(data);
+        jQuery.fx.off = true;
         let categoriesContainer = $(".categories > div > div:nth-child(2)");
         checkOnResize(categoriesContainer,"container", "container-fluid ");
         checkOnResize(categoriesContainer.find("> div"),"row", "row flex-row flex-nowrap");
