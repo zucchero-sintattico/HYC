dd098b642427a8ccc55f3de450a2ab5ceb1b2994
StefanoScolari <stefano.scolari@studio.unibo.it>
2022-01-02 09:22:49 +0100

    aloha animation time

--- Sito/script/pages/index.js
+++ Sito/script/pages/index.js
@@ -14,6 +14,26 @@ $(document).on("ready", function (event) {
         checkOnResize(categoriesContainer,"container", "container-fluid ");
         checkOnResize(categoriesContainer.find("> div"),"row", "row flex-row flex-nowrap");
 
+        let scrollTimeout = null;
+        let prevScrollTop = 0;
+        $(window).on("scroll", function(){
+            clearTimeout(scrollTimeout);
+            let actualScrollTop = $(this).scrollTop();
+            console.log("actual->"+actualScrollTop)
+            console.log("prev->"+prevScrollTop)
+            scrollTimeout = setTimeout(function (){
+                    $(".productWhole").each(function(){
+                        let randomValue = Math.random() * 30;
+                        if(prevScrollTop < actualScrollTop){
+                            randomValue = -randomValue;
+                        }
+                        $(this)[0].animate([{transform: `translateY(${randomValue}px)`,easing:"ease-out"},{transform: `translateY(0)`,easing:"ease-out"}],{duration: 400})
+                    });
+
+            prevScrollTop = actualScrollTop;
+            }, 50);
+        })
+
         window.setTimeout(() => {
             modifyIndexIfMobile();
         }, 600);
