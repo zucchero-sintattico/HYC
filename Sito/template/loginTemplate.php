431a6a5d3f1ea867b04af58f28ec465a600a47ab
Luigi Borriello - luigi.borriello2@studio.unibo.it <luigi.borriello2@studio.unibo.it>
2022-01-03 23:09:22 +0100

    restyle Login

--- Sito/template/loginTemplate.php
+++ Sito/template/loginTemplate.php
@@ -1,25 +1,19 @@
 <div class="row justify-content-center ">
-    <form action="#" method="POST">
+    <form>
         <h2>Login</h2>
-        <?php if (isset($templateParams["errorelogin"])): ?>
-            <p><?php echo $templateParams["errorelogin"]; ?></p>
-        <?php endif; ?>
-        <ul>
-            <li>
-                <label htmlFor="username">Username:</label><input type="text" id="username"
-                                                                  name="username"/>
-            </li>
-            <li>
-                <label htmlFor="password">Password:</label><input type="password" id="password"
-                                                                  name="password"/>
-            </li>
-            <li>
-                <input type="submit" name="submit" value="Invia"/>
-            </li>
-        </ul>
+
+        <label for="username">Username</label>
+        <input type="text" placeholder="Email or Phone" id="username">
+
+        <label for="password">Password</label>
+        <input type="password" placeholder="Password" id="password">
+
+        <input type="submit" value="Login">
+        <div class="row justify-content-center">
+            <a href="../register.php">Sign Up</a>
+        </div>
     </form>
+
 </div>
 
-<div class="row justify-content-center">
-    <a href="../register.php">Sign Up</a>
-</div>
+
