23795c557a96168abcc68cd7c13ddb2e546e7ba9
Luigi Borriello - luigi.borriello2@studio.unibo.it <luigi.borriello2@studio.unibo.it>
2022-01-04 21:01:54 +0100

    add check in api-checkout

--- Sito/API/api-checkout.php
+++ Sito/API/api-checkout.php
@@ -2,22 +2,29 @@
 
 require_once '../bootstrap.php';
 
-// Create the order
-$dbh -> createOrder(getLoggedUserID());
+
+if (isUserLoggedIn()) {
+    // Create the order
+    $dbh->createOrder(getLoggedUserID());
 
 // Get Order Info
-$orderInfo = $dbh -> getLastOrderOfUser(getLoggedUserID());
+    $orderInfo = $dbh->getLastOrderOfUser(getLoggedUserID());
 
 // Create a notification
-$dbh -> createNotification("Order Processed", 'Your order #'.$orderInfo['IdOrdine'].' was processed correctly, go to <a href="../profile.php">Profile</a> to check it! ', getLoggedUserID());
+    $dbh->createNotification("Order Processed", 'Your order #' . $orderInfo['IdOrdine'] . ' was processed correctly, go to <a href="../profile.php">Profile</a> to check it! ', getLoggedUserID());
 // Create a notification
-$dbh -> createNotification("Order Shipped", 'Your order #'.$orderInfo['IdOrdine'].' was leaved, go to <a href="../profile.php">Profile</a> to check it! ', getLoggedUserID());
+    $dbh->createNotification("Order Shipped", 'Your order #' . $orderInfo['IdOrdine'] . ' was leaved, go to <a href="../profile.php">Profile</a> to check it! ', getLoggedUserID());
 // Create Shipping ????
 
 // Give a new cart to the User
-$dbh -> getNewCartForUser(getLoggedUserID());
+    $dbh->getNewCartForUser(getLoggedUserID());
 
-$userName = getNameUserID();
-$data = array("UserName"=>$userName, "OrderInfo"=>$orderInfo);
-header('Content-Type: application/json');
-echo json_encode($data);
+    $userName = getNameUserID();
+    $data = array("UserName" => $userName, "OrderInfo" => $orderInfo);
+    header('Content-Type: application/json');
+    echo json_encode($data);
+}
+else{
+    echo("Not logged in");
+}
+
