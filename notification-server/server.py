7d1c94e95fccd03f1a66c12020058c0beabed8a1
Ro0t-set <tommaso.patriti@studio.unibo.it>
2022-01-04 23:16:04 +0100

    oreder traking

--- notification-server/server.py
+++ notification-server/server.py
@@ -8,12 +8,33 @@ from mysql.connector import  Error
 import time
 from threading import Thread
 import asyncio
+from datetime import datetime
 
 
 
 class MyWebSocketServer(tornado.websocket.WebSocketHandler):
     thread = None
     stop_threads = True
+    def make_notification(self, id, type, description):
+        mydb = mysql.connector.connect(
+          host="localhost",
+          user="root",
+          password="password",
+          database='HYC',
+          use_pure=False
+        )
+        cursor = mydb.cursor()
+        try:
+            now = datetime.today().strftime('%Y%m%d%H%M%S')
+            cursor.execute("USE HYC")
+            cursor.execute("INSERT INTO Notifica (TipoNotifica, Data, Descrizione, IdUtente, Letto) VALUES ((%s), (%s), (%s) , (%s), 0)", (type, now, description, id,))
+            mydb.commit()
+        except Error as e:
+                print("ERROR", e)
+        cursor.close()
+        mydb.close()
+
+
     def update_db(self, id):
         mydb = mysql.connector.connect(
           host="localhost",
@@ -48,7 +69,15 @@ class MyWebSocketServer(tornado.websocket.WebSocketHandler):
                 if notification != self.update_db(id):
                     notification = self.update_db(id)
                     self.write_message("update_notification")
+                    if notification[1] == "Order Processed":
+                        time.sleep(4)
+                        self.make_notification(id, "Order Shipped", "Your order has been  shipped")
+                    elif notification[1] == "Order Shipped":
+                        time.sleep(4)
+                        self.make_notification(id, "Order Delivered", "Order successfully delivered, go to <a href='../profile.php'>Profile</a> to check it")
                 time.sleep(3)
+
+
 
     def open(self):
         print('new connection')
