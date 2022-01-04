304d6fe6343ea379478e77b93a495d61ecb29b7d
StefanoScolari <stefano.scolari@studio.unibo.it>
2022-01-04 21:33:25 +0100

    Added thread in php for notification on order

--- Sito/API/api-checkout.php
+++ Sito/API/api-checkout.php
@@ -3,6 +3,25 @@
 
 require_once '../bootstrap.php';
 
+class workerThread extends Thread {
+
+    private $notificationTypeOrdered = array("OrderProcessed","OrderReceived", "OrderPrepared", "OrderShipped", "OrderDelivered");
+    private $dataToSendBackOrdered = array(" has been processed correctly", " has been received", " is being prepared", " shipped", " delivered");
+    private $userID;
+
+    public function __construct($id){
+        $this->userID=$id;
+    }
+
+    public function run(){
+        for($i=0;$i<count($this->notificationTypeOrdered);$i++){
+            $dbh->createNotification($this->notificationTypeOrdered[$i], 'Your order N#'.$orderInfo['IdOrdine'].$this->dataToSendBackOrdered[$i].', go to <a href="../profile.php">Profile</a> to check it! ', $this->userID);
+            sleep(6);
+        }
+    }
+}
+
+
 // Create the order
 $dbh->createOrder(getLoggedUserID());
 
@@ -10,8 +29,9 @@ $dbh->createOrder(getLoggedUserID());
 $orderInfo = $dbh->getLastOrderOfUser(getLoggedUserID());
 
 // Create a notification
-$dbh->createNotification("Order Processed", 'Your order #' . $orderInfo['IdOrdine'] . ' was processed correctly, go to <a href="../profile.php">Profile</a> to check it! ', getLoggedUserID());
-
+//$dbh->createNotification("OrderProcessed", 'Your order N#' . $orderInfo['IdOrdine'] . ' was processed correctly, go to <a href="../profile.php">Profile</a> to check it! ', getLoggedUserID());
+$worker = new workerThread(getLoggedUserID());
+$worker->start();
 
 // Give a new cart to the User
 $dbh->getNewCartForUser(getLoggedUserID());
