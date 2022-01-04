88d6d5838669d4202fc24fd69ffe0a4402e15a62
Ro0t-set <tommaso.patriti@studio.unibo.it>
2022-01-05 00:10:45 +0100

    oreder traking

---
+++ notification-server/notification_generetor.py
@@ -0,0 +1,69 @@
+import mysql.connector
+from mysql.connector import  Error
+import time
+import asyncio
+
+
+def make_notification(id, type, description):
+    mydb = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      password="password",
+      database='HYC',
+      use_pure=False
+    )
+    cursor = mydb.cursor()
+    try:
+        now = datetime.today().strftime('%Y%m%d%H%M%S')
+        cursor.execute("USE HYC")
+        cursor.execute("INSERT INTO Notifica (TipoNotifica, Data, Descrizione, IdUtente, Letto) VALUES ((%s), (%s), (%s) , (%s), 0)", (type, now, description, id,))
+        mydb.commit()
+    except Error as e:
+            print("ERROR", e)
+    cursor.close()
+    mydb.close()
+
+
+def update_db():
+    mydb = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      password="password",
+      database='HYC',
+      use_pure=False
+    )
+    cursor = mydb.cursor()
+    try:
+        cursor.execute("USE HYC")
+
+        cursor.execute("SELECT * FROM `Notifica`")
+        row = cursor.fetchone()
+        while row is not None:
+            return row
+
+    except Error as e:
+            print("ERROR", e)
+
+    cursor.close()
+    mydb.close()
+
+
+
+
+def thread_update_db():
+        notification = update_db()
+        while(1):
+            if notification != update_db():
+                notification = update_db()
+                user = notification[4]
+                if notification[1] == "Order Processed":
+                    time.sleep(4)
+                    make_notification(user, "Order Shipped", "Your order has been  shipped")
+                elif notification[1] == "Order Shipped":
+                    time.sleep(4)
+                    make_notification(user, "Order Delivered", "Order successfully delivered, go to <a href='../profile.php'>Profile</a> to check it")
+            time.sleep(3)
+
+
+
+thread_update_db()
