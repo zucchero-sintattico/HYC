09a118bf26784221662f9080457886554894150b
Ro0t-set <tommaso.patriti@studio.unibo.it>
2022-01-09 02:11:29 +0100

    validation

--- Sito/template/adminTemplate.php
+++ Sito/template/adminTemplate.php
@@ -8,49 +8,50 @@
             <a href="../editor-admin.php?mode=add">Add a Square</a>
 
         </div>
-    </section>
 
-    <section>
-        <h2>Product List</h2>
-        <div class="container">
-            <div class="row">
-                <script>quadri = []</script>
-                <?php foreach ($templateParams["products"] as $i => $square): ?>
-                    <div class="m-3 col">
-                        <div id="square<?php echo $i ?>">
-                            <script>
-                                quadri.push(new CodeSquare(document.querySelector('#square<?php echo $i ?>')));
-                                quadri[<?php echo $i?>].getSquare();
-                                quadri[<?php echo $i?>].setWidth(<?php echo $square['Larghezza'] ?>);
-                                quadri[<?php echo $i?>].setStyle('<?php echo $square['NomeTema'] ?>');
-                                quadri[<?php echo $i?>].setHeight(<?php echo $square['Altezza'] ?>);
-                                quadri[<?php echo $i?>].widthScale(270);
-                                quadri[<?php echo $i?>].setFramecolor('<?php echo $square['Colore_frame'] ?>')
-                                quadri[<?php echo $i?>].updateStyle();
-                                quadri[<?php echo $i?>].setText(<?php echo $square['Codice'] ?>);
-                                quadri[<?php echo $i?>].disable();
 
-                                /**validation code**/
-                                $("span").removeAttr("cm-text");
-                                $(".CodeMirror textarea").attr("title", "code main frame area").removeAttr("autocorrect");
-                                $(".CodeMirror span").removeAttr("cm-text");
-                                $(".CodeMirror div").removeAttr("cm-not-content").removeAttr("autocorrect");
+        <section>
+            <h2>Product List</h2>
+            <div class="container">
+                <div class="row">
+                    <script>quadri = []</script>
+                    <?php foreach ($templateParams["products"] as $i => $square): ?>
+                        <div class="m-3 col">
+                            <div id="square<?php echo $i ?>">
+                                <script>
+                                    quadri.push(new CodeSquare(document.querySelector('#square<?php echo $i ?>')));
+                                    quadri[<?php echo $i?>].getSquare();
+                                    quadri[<?php echo $i?>].setWidth(<?php echo $square['Larghezza'] ?>);
+                                    quadri[<?php echo $i?>].setStyle('<?php echo $square['NomeTema'] ?>');
+                                    quadri[<?php echo $i?>].setHeight(<?php echo $square['Altezza'] ?>);
+                                    quadri[<?php echo $i?>].widthScale(270);
+                                    quadri[<?php echo $i?>].setFramecolor('<?php echo $square['Colore_frame'] ?>')
+                                    quadri[<?php echo $i?>].updateStyle();
+                                    quadri[<?php echo $i?>].setText(<?php echo $square['Codice'] ?>);
+                                    quadri[<?php echo $i?>].disable();
 
-                            </script>
-                        </div>
-                        <div class="row">
-                            <div class="col">
-                                <p><?php echo $square['Titolo'] ?></p>
-                                <p><?php echo $square['Altezza'] ?>x<?php echo $square['Larghezza'] ?></p>
-                                <a href="../editor-admin.php?mode=edit&id=<?php echo $square['IdProd'] ?>">Edit</a>
-                                <label> | </label>
-                                <a href="../editor-admin.php?mode=del&id=<?php echo $square['IdProd'] ?>">Delete</a>
+                                    /**validation code**/
+                                    $("span").removeAttr("cm-text");
+                                    $(".CodeMirror textarea").attr("title", "code main frame area").removeAttr("autocorrect");
+                                    $(".CodeMirror span").removeAttr("cm-text");
+                                    $(".CodeMirror div").removeAttr("cm-not-content").removeAttr("autocorrect");
+
+                                </script>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <p><?php echo $square['Titolo'] ?></p>
+                                    <p><?php echo $square['Altezza'] ?>x<?php echo $square['Larghezza'] ?></p>
+                                    <a href="../editor-admin.php?mode=edit&id=<?php echo $square['IdProd'] ?>">Edit</a>
+                                    <label> | </label>
+                                    <a href="../editor-admin.php?mode=del&id=<?php echo $square['IdProd'] ?>">Delete</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                <?php endforeach; ?>
+                    <?php endforeach; ?>
+                </div>
             </div>
-        </div>
+        </section>
     </section>
 </div>
